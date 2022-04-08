821a1bab3216723a493ce19624b2b5ed826e321e
aigulsharip <aigul.sharip@nu.edu.kz>
2022-04-08 11:18:22 +0600

    Working on getPostDetails()...

--- Topic4/post-office-api/src/main/java/kz/daracademy/mapper/PostMapper.java
+++ Topic4/post-office-api/src/main/java/kz/daracademy/mapper/PostMapper.java
@@ -5,18 +5,17 @@ import kz.daracademy.dto.PostDTO;
 import kz.daracademy.model.ClientResponse;
 import kz.daracademy.model.PostModel;
 import kz.daracademy.model.PostResponse;
-import kz.daracademy.service.PostService;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
 
-    PostDTO postToDTO (PostResponse postResponse);
-    ClientDTO clientToDTO (ClientResponse clientResponse);
+    PostDTO postToDTO(PostResponse postResponse);
+
+    ClientDTO clientToDTO(ClientResponse clientResponse);
+
     List<PostDTO> postsListToDto(List<PostModel> posts);
 
     //ItemDTO itemToDto(Item item);
