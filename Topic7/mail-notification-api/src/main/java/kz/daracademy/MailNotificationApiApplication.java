f94791eac6dbebffd76da5bf9f8c8a78246c0684
aigulsharip <aigul.sharip@nu.edu.kz>
2022-05-04 16:32:10 +0600

    Added Kafka configs to client-payment, created mail-notification-api

---
+++ Topic7/mail-notification-api/src/main/java/kz/daracademy/MailNotificationApiApplication.java
@@ -0,0 +1,15 @@
+package kz.daracademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MailNotificationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailNotificationApiApplication.class, args);
+    }
+
+}
